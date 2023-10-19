de1089ae7ad5c788341db082816c1342680bfdd1
= <=>
2023-10-19 15:33:02 +0330

    Refactoed MenuItem component using cn utility

--- src/components/Header/MenuItem.tsx
+++ src/components/Header/MenuItem.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from "react";
+import { cn } from "../../lib/utils";
 
 const MenuItem = ({
   children,
@@ -13,9 +14,10 @@ const MenuItem = ({
     <li className="group cursor-pointer text-sm font-light relative border-2 border-gray-300 rounded-full px-2 py-4">
       {children}
       <div
-        className={`${
+        className={cn(
+          "group-hover:opacity-100 absolute left-1/2 -translate-x-1/2 bottom-2 w-1 h-1 transition-all rounded-full",
           !isActive && "opacity-0"
-        } group-hover:opacity-100 absolute left-1/2 -translate-x-1/2 bottom-2 w-1 h-1 transition-all rounded-full`}
+        )}
         style={{ backgroundColor: dotColor }}
       ></div>
     </li>
