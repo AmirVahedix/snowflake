69cb7fe8ad6546b97232fed5667889a3e65e38b5
= <=>
2023-10-19 15:19:04 +0330

    Added hover dot in header menu

--- src/components/Header/MenuItem.tsx
+++ src/components/Header/MenuItem.tsx
@@ -2,8 +2,9 @@ import { PropsWithChildren } from "react";
 
 const MenuItem = ({ children }: PropsWithChildren) => {
   return (
-    <li className="text-sm font-light border-2 border-gray-300 rounded-full px-2 py-4">
+    <li className="group cursor-pointer text-sm font-light relative border-2 border-gray-300 rounded-full px-2 py-4">
       {children}
+      <div className="absolute left-1/2 -translate-x-1/2 bottom-2 w-1 h-1 bg-white group-hover:bg-black transition rounded-full"></div>
     </li>
   );
 };
