9765543bb12b4fd902402941a384cc0184f7822c
= <=>
2023-10-20 15:06:46 +0330

    Added hover state to slider actions and navigation

--- src/components/Slider/SliderActionButton.tsx
+++ src/components/Slider/SliderActionButton.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const SliderActionButton = ({ icon }: { icon: ReactNode }) => {
   return (
-    <button className="w-14 h-14 flex items-center backdrop-blur justify-center rounded-full bg-gray-300 bg-opacity-60">
+    <button className="w-14 h-14 flex items-center backdrop-blur justify-center rounded-full bg-gray-300/60 hover:bg-gray-400/50 transition">
       <span className="text-2xl text-gray-800">{icon}</span>
     </button>
   );
--- src/components/Slider/SliderNavigation.tsx
+++ src/components/Slider/SliderNavigation.tsx
@@ -3,10 +3,10 @@ import { FiArrowDownLeft, FiArrowUpRight } from "react-icons/fi";
 const SliderNavigation = () => {
   return (
     <div className="absolute top-56 -left-14 flex flex-col items-center">
-      <button className="w-12 h-12 flex items-center justify-center rounded-full border-[2.3px] border-gray-300">
+      <button className="w-12 h-12 flex items-center justify-center rounded-full border-[2.3px] border-gray-300 hover:border-gray-400 transition">
         <FiArrowUpRight className="text-xl" />
       </button>
-      <button className="w-12 h-12 flex items-center justify-center rounded-full border-[2.3px] border-gray-300">
+      <button className="w-12 h-12 flex items-center justify-center rounded-full border-[2.3px] border-gray-300 hover:border-gray-400 transition">
         <FiArrowDownLeft className="text-xl" />
       </button>
     </div>
