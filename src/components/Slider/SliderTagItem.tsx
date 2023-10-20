a2fec3cae6cd28b27af90bf28f73e92e6974bc76
= <=>
2023-10-20 21:46:01 +0330

    Handle slider tags state

--- src/components/Slider/SliderTagItem.tsx
+++ src/components/Slider/SliderTagItem.tsx
@@ -4,18 +4,21 @@ import { cn } from "../../lib/utils";
 const SliderTagItem = ({
   children,
   isActive = false,
+  onClick,
 }: {
   children: ReactNode;
   isActive?: boolean;
+  onClick: () => void;
 }) => {
   return (
     <li
       className={cn(
-        "rounded-full border-[1.5px] flex items-center px-3 py-[2px] transition cursor-pointer",
+        "rounded-full border-[1.5px] flex items-center px-3 py-[2px] transition-all cursor-pointer",
         isActive
           ? "text-lg text-gray-700 border-gray-400"
           : "text-sm text-gray-500 hover:text-gray-700 border-gray-300"
       )}
+      onClick={onClick}
     >
       {children}
     </li>
--- src/components/Slider/SliderTags.tsx
+++ src/components/Slider/SliderTags.tsx
@@ -1,14 +1,59 @@
+import { useState } from "react";
 import SliderTagItem from "./SliderTagItem";
 
 const SliderTags = () => {
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+
   return (
     <ul className="flex flex-row flex-wrap items-end w-full justify-end gap-x-2 gap-y-[6px]">
-      <SliderTagItem>Table</SliderTagItem>
-      <SliderTagItem isActive>Sofa</SliderTagItem>
-      <SliderTagItem>Cupboard</SliderTagItem>
-      <SliderTagItem>Bookshelf</SliderTagItem>
-      <SliderTagItem>Chair</SliderTagItem>
-      <SliderTagItem>Bed</SliderTagItem>
+      <SliderTagItem
+        isActive={activeIndex == 0}
+        onClick={() => {
+          setActiveIndex(0);
+        }}
+      >
+        Table
+      </SliderTagItem>
+      <SliderTagItem
+        isActive={activeIndex == 1}
+        onClick={() => {
+          setActiveIndex(1);
+        }}
+      >
+        Sofa
+      </SliderTagItem>
+      <SliderTagItem
+        isActive={activeIndex == 2}
+        onClick={() => {
+          setActiveIndex(2);
+        }}
+      >
+        Cupboard
+      </SliderTagItem>
+      <SliderTagItem
+        isActive={activeIndex == 3}
+        onClick={() => {
+          setActiveIndex(3);
+        }}
+      >
+        Bookshelf
+      </SliderTagItem>
+      <SliderTagItem
+        isActive={activeIndex == 4}
+        onClick={() => {
+          setActiveIndex(4);
+        }}
+      >
+        Chair
+      </SliderTagItem>
+      <SliderTagItem
+        isActive={activeIndex == 5}
+        onClick={() => {
+          setActiveIndex(5);
+        }}
+      >
+        Bed
+      </SliderTagItem>
     </ul>
   );
 };
