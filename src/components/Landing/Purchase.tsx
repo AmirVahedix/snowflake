9c61e3ac21e775189903a534d53f58edd978fbae
= <=>
2023-10-20 12:10:03 +0330

    Added cart and buy buttons

--- src/components/Landing/Purchase.tsx
+++ src/components/Landing/Purchase.tsx
@@ -1,3 +1,5 @@
+import { FiShoppingBag } from "react-icons/fi";
+
 import SizeSelector from "./SizeSelector";
 import Toggles from "./Toggles";
 
@@ -6,6 +8,14 @@ const Purchase = () => {
     <div className="mt-8 flex items-start gap-4">
       <SizeSelector />
       <Toggles />
+      <div className="flex h-full py-1">
+        <button className="h-full w-10 border border-gray-400 flex items-center justify-center rounded-full">
+          <FiShoppingBag className="text-gray-700 text-xl" />
+        </button>
+        <button className="w-32 rounded-full text-gray-800 h-full flex items-center justify-center border border-gray-400">
+          Buy
+        </button>
+      </div>
     </div>
   );
 };
